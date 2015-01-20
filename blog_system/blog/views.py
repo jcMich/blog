970c36c56bb2f456cb8397f15c1df34720788e29
jcMich <jc.fie.umich@gmail.com>
2015-01-20 11:45:54 -0600

    comentarios

--- blog_system/blog/views.py
+++ blog_system/blog/views.py
@@ -104,7 +104,7 @@ class BlogDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         ctx = super(BlogDetail, self).get_context_data(**kwargs)
-        ctx['comentarios'] = comentarios.objects.filter(Blog__id=self.kwargs['pk'])
+        ctx['comentarios'] = comentarios.objects.filter(Blog__id=self.kwargs['pk']).order_by('-fecha_pub')
         ctx['comentariosForm'] = ComentarioForm()
         return ctx
 
